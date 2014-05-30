ebfa6cc9ec771063ba68e7cc2642f6e8f1c35481
Sarah <semurray3@gmail.com>
2014-05-30 04:02:58 -0500

    Projects page overhaul, added wookmark jquery thing

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,6 +8,13 @@ function responsivize() {
 
 $(document).ready(function() {
   
+  $('#tiles li').wookmark({
+		autoResize: true, // This will auto-update the layout when the browser window is resized.
+		container: $('#tiles'), // Optional, used for some extra CSS styling
+		offset: 3, // Optional, the distance between grid items
+		itemWidth: 300 // Optional, the width of a grid item
+	});
+  
   $('.featured-image').imgLiquid();
   responsivize();
   $(window).on('resize', responsivize);
